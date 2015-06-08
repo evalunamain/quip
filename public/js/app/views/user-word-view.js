6046d6d04ea3711cac10f15dc67ceef9336df450
Eva Main <evalunamain@gmail.com>
2015-06-07 22:49:01 -0400

    Basic css

--- public/js/app/views/user-word-view.js
+++ public/js/app/views/user-word-view.js
@@ -2,7 +2,7 @@ define(['marionette'], function(Marionette) {
 
     var UserWordView = Marionette.ItemView.extend({
 
-    	className: 'small-4 columns userword',
+    	className: 'small-4 columns',
 
         template: '#userword',
 
